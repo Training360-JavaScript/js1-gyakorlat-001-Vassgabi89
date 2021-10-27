a4e784aeb6717a3747652ef84ab404fe50c64b0a
Gábor Vass <90134776+Vassgabi89@users.noreply.github.com>
2021-10-27 22:56:42 +0200

    maybe now?

--- js/person.js
+++ js/person.js
@@ -1,3 +1,3 @@
 'use strict';
 
-const personDataLog = (Person = {firstName:'John', lastName:'Doe', age:33}) => 'My name is ' + person.firstName + ' ' + person.lastName + '. I\'m ' + person.age + ' years old.';
+const personDataLog = (person={firstName:"John", lastName:"Doe", age:33}) => console.log ('My name is ' + person.firstName + ' ' + person.lastName + '. I\'m ' + person.age + ' years old.');
