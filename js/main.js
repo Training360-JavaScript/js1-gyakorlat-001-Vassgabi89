982b876fe055745dfe97d47159107478efd95586
Gábor Vass <90134776+Vassgabi89@users.noreply.github.com>
2021-10-27 22:19:16 +0200

    again

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 'use strict';
 
-let buttons= document.querySelectorAll('.button');
+let buttons = document.querySelectorAll('.button');
 
-const handleClick = function () {
-    for (let i = 0; i < buttons.length; i++) {
+const handleClick = () => {
+    for (let i = 0; i < buttons.length; i+=1)
+    {
         buttons[i].addEventListener('click', function(){
-            console.log (i);
-        }); 
+            console.log (this.innerHTML);
+        }) 
 }
 }
 
--- js/sum.js
+++ js/sum.js
@@ -2,4 +2,4 @@
 
 const summation = (a=0,b=0) => a + b;
 
-const subtraction = (a=0,b=0) => a + b;
+const subtraction = (a=0,b=0) => a - b;
