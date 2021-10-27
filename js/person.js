3e4402d5b6d169dedea5fae0edf5ed27bc75375f
Gábor Vass <90134776+Vassgabi89@users.noreply.github.com>
2021-10-27 23:36:19 +0200

    2. teszten kívül minden jó, de tényleg

--- js/person.js
+++ js/person.js
@@ -1,3 +1,3 @@
 'use strict';
 
-const personDataLog = (person={firstname:'John', lastname:'Doe', age:33}) => console.log ('My name is ' + person.firstname + ' ' + person.lastname + '. I\'m ' + person.age + ' years old.');
+const personDataLog = (person={firstname:'John', lastname:'Doe', age:33}) => ('My name is ' + person.firstname + ' ' + person.lastname + '. I\'m ' + person.age + ' years old.');
